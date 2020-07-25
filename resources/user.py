2fc2a7e048fa138a54f4f960aa139134acc88456
praj98 <praj98730@gmail.com>
2020-07-25 17:42:40 +0530

    Added new flask api

---
+++ resources/user.py
@@ -0,0 +1,15 @@
+import sqlite3
+from flask_restful import Resource,reqparse
+from models.user import UserModel
+class UserRegister(Resource):
+    parser=reqparse.RequestParser()
+    parser.add_argument('username',type=str,required=True,help="This cannot be blank")
+    parser.add_argument('password',type=str,required=True,help="This cannot be blank")
+    def post(self):
+        data=UserRegister.parser.parse_args()
+        if UserModel.find_by_user(data['username']):
+            return {"Message":"A username already exsist"},400
+        user=UserModel(**data)
+        user.save_to_db()
+
+        return {'Message':'User created successfully'},201
